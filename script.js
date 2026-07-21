614c6bf2bb92663e340bc87335d88732bb7b8d31
164088-eng <164088@wsk.ac.th>
2026-07-21 21:23:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+*{
+margin:0;
+
+padding:0;
+
+box-sizing:border-box;
+
+scroll-behavior: smooth;
+
+font-family:Prompt, sans-serif;
+
+body{
+
+background:#f4faf6;
+
+70%
+
+github.com
+
+position:fixed;
+
+17
+
+width:100%;
+
+19
+
+display: flex;
+
+21
+
+justify-content: ween;
+
+22
+
+23 padding: 20px 60px;
+
+25
+
+background:white;
+
+26
+
+27
+
+box-shadow: 5px 20px rgba(0,0,0,.08);
